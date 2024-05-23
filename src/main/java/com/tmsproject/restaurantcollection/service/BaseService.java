37df7e8d0ec6608381cb54f066eeffd2bbcb13d3
tendernesc <122694439+tendernesc@users.noreply.github.com>
2024-05-23 10:10:17 +0300

    add BaseService

---
+++ src/main/java/com/tmsproject/restaurantcollection/service/BaseService.java
@@ -0,0 +1,14 @@
+package com.tmsproject.restaurantcollection.service;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseService<T> {
+    List<T> findAll();
+    Optional<T> findById(Long id);
+    T create(T t);
+    T update(T t);
+    void delete(Long id);
+    void delete(T entity);
+
+}
