7eb02e3c16c3c8573bcc02da6d9e4a95374d7666
Ivan Koval <AlfaAndOmega789@gmail.com>
2024-05-23 11:34:25 +0300

    add BaseEntity

---
+++ src/main/java/com/tmsproject/restaurantcollection/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.tmsproject.restaurantcollection.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+import static com.tmsproject.restaurantcollection.domain.ResIdSeq.RES_SEQUENCE_NAME;
+import static com.tmsproject.restaurantcollection.domain.ResIdSeq.RES_SEQ_GENERATOR;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+@SuperBuilder
+public class BaseEntity {
+    @Id
+    @SequenceGenerator(name = RES_SEQ_GENERATOR, sequenceName = RES_SEQUENCE_NAME, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = RES_SEQ_GENERATOR)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+}
